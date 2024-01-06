c749efabd1e485a98817a43bffa8f6943d042f57
Juan Diego Nasello <juandiiinasello@gmail.com>
2024-01-06 15:13:26 -0300

    añadiendo funcion toLowerCase y aplicandola en SearchPokemon.jsx

--- src/components/SearchPokemon.jsx
+++ src/components/SearchPokemon.jsx
@@ -2,7 +2,7 @@ import { Link, Outlet, useNavigate } from 'react-router-dom'
 import SearchIcon from '@mui/icons-material/Search';
 import { useContext } from 'react';
 import { PokemonContext } from '../context/Context';
-
+import { lowerCase } from '../functions/functions';
 
 const SearchPokemon = () => {
 
@@ -25,7 +25,7 @@ const SearchPokemon = () => {
                     <Link to='/pokedex'><h1 className='custom-font'>Pokedex</h1></Link>
                     <form onSubmit={searchSubmit}>
                         <input type="search" name='searchValue'
-                            onChange={inputChange} value={searchValue}
+                            onChange={inputChange} value={lowerCase(searchValue)}
                             placeholder='Search Pokemons' />
                         <button className='btn-search'>{<SearchIcon fontSize='small' style={{ color: '#fff' }} />}</button>
                     </form>
--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,3 +1,7 @@
 export const upperCase = (value) => {
     return value[0].toUpperCase() + value.substring(1)
 }
+
+export const lowerCase = (value) => {
+    return value.toLowerCase()
+}
