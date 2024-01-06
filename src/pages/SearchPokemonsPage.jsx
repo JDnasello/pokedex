bc5a820efd823ebcbc19d39166b8242a906d5577
Juan Diego Nasello <juandiiinasello@gmail.com>
2024-01-06 19:39:05 -0300

    cambios en titulo de la pagina y busqueda de pokemones

--- src/pages/SearchPokemonsPage.jsx
+++ src/pages/SearchPokemonsPage.jsx
@@ -6,17 +6,24 @@ import PokemonCard from "../components/PokemonCard"
 const SearchPokemonsPage = () => {
 
     const location = useLocation()
-    console.log(location);
+    console.log(location)
     const { allPokemons } = useContext(PokemonContext)
 
     const pokemonsFounded = allPokemons.filter(pokemon => pokemon.name.includes(location.state))
+    
 
     return (
         <div className="container-pokedex">
             <div className="container-pokemons-list">
-                {pokemonsFounded.map(pokemon => (
-                <PokemonCard key={pokemon.id} pokemon={pokemon} />
-                ))}
+                {pokemonsFounded.length > 0 ? 
+                    pokemonsFounded.map(pokemon => 
+                            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                        )
+                
+                    : 
+                        <span>Pokemons not found</span>
+                    
+                }
             </div>
         </div>
     )
